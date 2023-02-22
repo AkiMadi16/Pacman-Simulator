987274e83cd66887bf3a448a29cdd3edaab6e3ca
AkiMadie16 <madie86@gmail.com>
2023-02-22 12:02:22 +1030

    clearing up code

--- pacman.js
+++ pacman.js
@@ -8,130 +8,89 @@ var placeUserInput = document.querySelector("#placePacMan");
 var showCurrentLocation = document.querySelector("#currentLocation");
 var box = document.querySelector(".box");
 
-// console.log(userInput);
-// let validInput = /^[PLACE][0-4],[0-4],(NORTH|EAST|SOUTH|WEST)$/.test(userInput);
+var placeX, placeY, placeF;
+
+var directions = [
+  {
+    face: "NORTH",
+    angle: "0deg",
+  },
+  { face: "EAST", angle: "90deg" },
+  { face: "SOUTH", angle: "180deg" },
+  { face: "WEST", angle: "270deg" },
+];
+var facingDirection = directions
+  .filter((a) => a.face === placeF)
+  .map((b) => b.angle);
 
 placeCommandButton.addEventListener("click", function (event) {
-  box = event.target;
-  console.log(box.textContent);
-  // document.querySelector("#b21").textContent = "";
+  if (placeX !== undefined && placeY !== undefined && placeF !== undefined) {
+    document.querySelector(
+      `#x${placeX}y${placeY}`
+    ).textContent = `${placeX},${placeY}`;
+
+    document.querySelector(
+      `#x${placeX}y${placeY}`
+    ).style.transform = `rotate(${facingDirection})`;
+  }
+
   let userInput = placeUserInput.value;
   let coordinates = userInput.match(/[0-4]+/g);
   let facingDirection = userInput
     .substring(userInput.length - 5)
-    .match(/(NORTH|EAST|SOUTH|WEST)/g)
-    .join("");
+    .match(/(NORTH|EAST|SOUTH|WEST)/g)[0];
 
-  if (userInput) {
-    // console.log(coordinates);
-    let coordinatesOfBox = `x${coordinates[0]}y${coordinates[1]}`;
-    // console.log(coordinatesOfBox);
-    let box = document.querySelector(`#${coordinatesOfBox}`);
+  placeX = Number(coordinates[0]);
+  placeY = Number(coordinates[1]);
+  placeF = facingDirection;
 
-    box.textContent = pacMan;
+  let coordinatesOfBox = `x${placeX}y${placeY}`;
+  // console.log(coordinatesOfBox);
+  let box = document.querySelector(`#${coordinatesOfBox}`);
+  box.textContent = pacMan;
+});
 
-    let directions = ["NORTH", "EAST", "SOUTH", "WEST"];
-    let rightAngle = [0, 90, 180, 270];
-    let leftAngle = [360, 270, 180, 90];
+moveCommand.addEventListener("click", function (event) {
+  event.preventDefault();
+  document.querySelector(
+    `#x${placeX}y${placeY}`
+  ).textContent = `${placeX},${placeY}`;
 
-    if (facingDirection === "NORTH") {
-      box.style.transform = `rotate(0deg)`;
-    }
-    if (facingDirection === "EAST") {
-      box.style.transform = `rotate(90deg)`;
-    }
-    if (facingDirection === "SOUTH") {
-      box.style.transform = `rotate(180deg)`;
-    }
-    if (facingDirection === "WEST") {
-      box.style.transform = `rotate(270deg)`;
-    }
+  if (placeF === "NORTH") {
+    placeY = placeY + 1;
+    box.style.transform = `rotate(0deg)`;
+  }
 
-    moveCommand.addEventListener("click", function (event) {
-      event.preventDefault();
-      newBox = event.target;
-      console.log(newBox);
-      box.textContent = "";
-      let count = 0;
-      if (pacMan && facingDirection === "NORTH") {
-        let newCoordinates = `x${coordinates[0]}y${Number(coordinates[1]) + 1}`;
-        let newBox = document.querySelector(`#${newCoordinates}`);
-        newBox.textContent = pacMan;
-        newBox.style.transform = `rotate(0deg)`;
+  if (placeF === "EAST") {
+    placeX = placeX + 1;
+    box.style.transform = `rotate(90deg)`;
+  }
+  if (placeF === "SOUTH") {
+    placeY = placeY - 1;
+    box.style.transform = `rotate(180deg)`;
+  }
+  if (placeF === "WEST") {
+    placeX = placeX - 1;
+    box.style.transform = `rotate(270deg)`;
+  }
 
-        count++;
-      }
+  placeX = placeX > 4 ? 4 : placeX;
+  placeX = placeX < 0 ? 0 : placeX;
+  placeY = placeY > 4 ? 4 : placeY;
+  placeY = placeY < 0 ? 0 : placeY;
 
-      if (pacMan && facingDirection === "EAST") {
-        let newCoordinates = `x${Number(coordinates[0]) + 1}y${coordinates[1]}`;
-        let newBox = document.querySelector(`#${newCoordinates}`);
-        newBox.textContent = pacMan;
-        newBox.style.transform = `rotate(90deg)`;
-      }
-      if (pacMan && facingDirection === "SOUTH") {
-        let newCoordinates = `x${coordinates[0]}y${Number(coordinates[1]) - 1}`;
-        let newBox = document.querySelector(`#${newCoordinates}`);
-        newBox.textContent = pacMan;
-        newBox.style.transform = `rotate(180deg)`;
-      }
-      if (pacMan && facingDirection === "WEST") {
-        let newCoordinates = `x${Number(coordinates[0]) - 1}y${coordinates[1]}`;
-        let newBox = document.querySelector(`#${newCoordinates}`);
-        newBox.textContent = pacMan;
-        newBox.style.transform = `rotate(270deg)`;
-      }
-    });
+  let newCoordinates = `x${placeX}y${placeY}`;
+  let newBox = document.querySelector(`#${newCoordinates}`);
+  newBox.textContent = pacMan;
+});
 
-    let boxes = document.querySelectorAll(".box");
-    rightCommand.addEventListener("click", function (event) {
-      for (let i = 0; i < boxes.length; i++) {
-        if (boxes[i].textContent === pacMan) {
-          boxes[i] = event.target;
-          // let sum = 0;
-          for (let i = 0; i <= directions.length; i++) {
-            if (directions[i] === `rotate(${rightAngle[i]}deg)`) {
-              boxes[i].style.transform = `rotate(${rightAngle[i + 1]}deg)`;
-              // sum += directions[i];
-            }
-          }
-        }
-      }
-    });
-
-    leftCommand.addEventListener("click", function (event) {
-      // let sum = 0;
-      for (let i = 0; i < boxes.length; i++) {
-        if (boxes[i].textContent === pacMan) {
-          boxes[i] = event.target;
-          // let sum = 0;
-          for (let i = 0; i <= directions.length; i++) {
-            if (directions[i] === `rotate(${leftAngle[i]}deg)`) {
-              boxes[i].style.transform = `rotate(${leftAngle[i + 1]}deg)`;
-              // sum += directions[i];
-            }
-          }
-        }
-      }
-    });
-
-    reportCommand.addEventListener("click", function (event) {
-      for (let i = 0; i < boxes.length; i++) {
-        if (boxes[i].textContent === pacMan) {
-          let boxCoordinate = boxes[i].id
-            .split("")
-            .join(",")
-            .match(/[0-4]/g)
-            .join(",");
-          document.createElement(
-            "h3"
-          ).textContent = `Output: ${boxCoordinate} ,${facingDirection} 
-          `;
-        }
-      }
-    });
-
-    if (!userInput) {
-      box.textContent = "";
+let boxes = document.querySelector(".box");
+rightCommand.addEventListener("click", function (event) {
+  event.preventDefault();
+  for (let i = 0; i < boxes.length; i++) {
+    if (boxes[i].textContent === pacMan) {
+      boxes[i] = event.target;
+      console.log(boxes[i]);
     }
   }
 });
--- pacman_test.js
+++ pacman_test.js
@@ -1,91 +1,61 @@
-const { NullConsole } = require("@jest/console");
-
-function placePacMan(array) {
-  // var moves = ["X", "y"];
-  let coordinates = array[0].match(/[0-4]+/g);
-  console.log(coordinates);
-  let facingDirection = array[0].substring(10);
-  console.log(facingDirection);
-
-  let validInput = /^[PLACE][0-4],[0-4],(NORTH|EAST|SOUTH|WEST)$/.test(userInput);
-
- 
 function placePacMan(userInput) {
+  let validInput = /^(PLACE)[0-4],[0-4],(NORTH|EAST|SOUTH|WEST)$/.test(
+    userInput[0]
+  );
+  let directions = ["NORTH", "EAST", "SOUTH", "WEST"];
+  let [X, Y, F] = userInput[0].split(" ")[1].split(",");
 
   if (validInput) {
-    let [x, y, f] = userInput.split(" ")[1].split(",").join(",");
     // ['2', '3', 'NORTH']
 
-    let command = ""
-    
-    switch (command) {
-      case 'MOVE':
-        case 'NORTH':
-          let northY = Number(y) + 1;
-          y = northY < 5 ? northY : 4
+    for (let i = 1; i <= userInput.length; i++) {
+      let command = "userInput[i]";
+
+      switch (command) {
+        case "MOVE":
+          switch (F) {
+            case "NORTH":
+              let northY = Number(Y) + 1;
+              Y = northY < 5 ? northY : 4;
+              break;
+            case "EAST":
+              let eastX = Number(X) + 1;
+              X = eastX < 5 ? eastX : 4;
+              break;
+            case "SOUTH":
+              let southY = Number(Y) - 1;
+              Y = southY < 5 ? southY : 4;
+              break;
+            case "WEST":
+              let westX = Number(X) - 1;
+              X = westX < 5 ? westX : 4;
+              break;
+          }
           break;
-          case 'EAST':
-          let eastX = Number(x) + 1;
-          x = eastX < 5 ? eastX : 4
+        case "RIGHT":
+          F = directions[(directions.indexOf(F) + 1) % directions.length];
           break;
-          case 'SOUTH':
-          let southY = Number(y) - 1;
-          y = southY < 5 ? southY : 4
+        case "LEFT":
+          F = directions.at(directions.indexOf(F) - 1);
           break;
-          case 'WEST':
-          let westX = Number(x) - 1;
-          x = westX < 5 ? westX : 4
-          break;
+      }
     }
-
-    case 'RIGHT': 
-
-
   }
-    // if (str[i] === "LEFT" && facingDirection === direction.face) {
-    //   let newCoordinates = coordinates;
-    //   let newRotation = direction
-    //     .filter((direct) => facingDirection === direct.face)
-    //     .map((a) => Number(a.rotation[1]) - 90)
-    //     .join("");
-    //   if (
-    //     newRotation === direction.rotation[0] ||
-    //     newFace === direction.rotation[1]
-    //   ) {
-    //     let newFace = direction.face;
-    //   }
-    // }
-
-    // if (str[i] === "RIGHT" && facingDirection === direction.face) {
-    //   let newCoordinates = coordinates;
-    //   let newRotation = direction
-    //     .filter((direct) => facingDirection === direct.face)
-    //     .map((a) => Number(a.rotation[0]) + 90)
-    //     .join("");
-    //   if (
-    //     newRotation === direction.rotation[0] ||
-    //     newFace === direction.rotation[1]
-    //   ) {
-    //     let newFace = direction.face;
-    //   }
-    // }
-
-    if (str[i].includes("REPORT")) {
-      console.log(newCoordinates);
-    }
-    // return "Output:" + newCoordinates + "," + newFace;
-  }
-  if (0 < coordinates[0] < 4 && 0 < coordinates[1] < 4) {
-    return "Output:" + newCoordinates + "," + newFace;
-  } else {
-    return "Output: error ";
-  }
+  return `Output: ${X}, ${Y}, ${F}`;
 }
+
+//   }
+//   if (0 < coordinates[0] < 4 && 0 < coordinates[1] < 4) {
+//     return "Output:" + newCoordinates + "," + newFace;
+//   } else {
+//     return "Output: error ";
+//   }
+// }
 console.log(placePacMan(["PLACE 0,0,NORTH", "MOVE", "REPORT"]));
-// console.log(placePacMan(["PLACE 0,0,NORTH", "LEFT", "REPORT"]));
-// console.log(
-//   placePacMan(["PLACE 1,2,EAST", "MOVE", "MOVE", "LEFT", "MOVE", "REPORT"])
-// );
+console.log(placePacMan(["PLACE 0,0,NORTH", "LEFT", "REPORT"]));
+console.log(
+  placePacMan(["PLACE 1,2,EAST", "MOVE", "MOVE", "LEFT", "MOVE", "REPORT"])
+);
 
 // choosing the box and iterare through F, b0 - "PLACE, b0 value, F keys value"
 
